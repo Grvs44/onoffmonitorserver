81ac37173befb211d560ef0a5851b277c0616763
Joe Greaves <74414406+Grvs44@users.noreply.github.com>
2023-07-14 14:44:21 +0100

    Add an action for creating a Status without returning the data

--- onoffmonitorserver/views.py
+++ onoffmonitorserver/views.py
@@ -1,6 +1,8 @@
 # pylint:disable=no-member
 from knox.auth import TokenAuthentication
+from rest_framework import status
 from rest_framework.authentication import SessionAuthentication
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
@@ -11,6 +13,9 @@ from .serializers import DeviceSerializer, StatusSerializer
 
 
 class DeviceViewSet(ModelViewSet):
+    """
+    ViewSet for managing Device objects
+    """
     queryset = models.Device.objects.all()
     serializer_class = DeviceSerializer
     permission_classes = [IsAuthenticated, IsDeviceOwner]
@@ -19,8 +24,21 @@ class DeviceViewSet(ModelViewSet):
 
 
 class StatusViewSet(ModelViewSet):
+    """
+    ViewSet for managing Status objects
+    """
     queryset = models.Status.objects.all()
     serializer_class = StatusSerializer
     permission_classes = [IsAuthenticated, IsStatusOwner]
     authentication_classes = [SessionAuthentication, TokenAuthentication]
     filter_backends = [StatusOwnerFilter]
+
+    @action(methods=['POST'], detail=False, url_path='n')
+    def create_no_return(self, request):
+        """
+        Create a Status object but don't return the created data
+        """
+        response = super().create(request)
+        response.data = None
+        response.status_code = status.HTTP_204_NO_CONTENT
+        return response
